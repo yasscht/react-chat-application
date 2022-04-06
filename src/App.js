292c69349e9ff9cd46b6c05d979fb98528d9048d
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-06 22:59:32 +0000

    integrating the chat engine and creating the environement variable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectId={process.env.PROJECT_ID}
+      userName={process.env.USER_NAME}
+      userSecret={process.env.USER_SECRET}
+    />
+  );
 }
 
 export default App;
