8e96982f4245d29f5276ac1e53f1b7332ad5dc50
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-06 23:23:58 +0000

    fixing the environement variable bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 function App() {
+  console.log(process.env.PROJECT_ID);
   return (
     <ChatEngine
       height="100vh"
-      projectId={process.env.PROJECT_ID}
-      userName={process.env.USER_NAME}
-      userSecret={process.env.USER_SECRET}
+      projectID={process.env.REACT_APP_PROJECT_ID}
+      userName={process.env.REACT_APP_USER_NAME}
+      userSecret={process.env.REACT_APP_USER_SECRET}
     />
   );
 }
