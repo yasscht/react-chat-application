195958c8b3aa68b2e44898237b76c11aaed409e3
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-06 23:57:07 +0000

    initializing the app sturcture

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,7 +4,36 @@ import TheirMessage from "./TheirMessage";
 const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
   const chat = chats && chats[activeChat];
-  console.log(chat, userName, messages);
-  return <div>chat feed</div>;
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : messages[index - 1];
+      const isMyMessage = userName === message.sender.username;
+      <div key={`msg_${index}`} style={{ width: "100%" }}>
+        <div className="message-block">
+          {isMyMessage ? <MyMessage /> : <TheirMessage />}
+        </div>
+        <div
+          className="read-receipts"
+          style={{
+            marginRight: isMyMessage ? "18px" : "0px",
+            marginLeft: isMyMessage ? "0px" : "68px",
+          }}
+        >
+          message receipts
+        </div>
+      </div>;
+    });
+  };
+  renderMessages();
+  if (!chat) return "...Loading";
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+      </div>
+    </div>
+  );
 };
 export default ChatFeed;
