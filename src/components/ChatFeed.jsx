ceffcdf8519846e2b66522708eec87529ca7c77d
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-07 11:00:04 +0000

    build the MyMessage component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -12,7 +12,14 @@ const ChatFeed = (props) => {
       const isMyMessage = userName === message.sender.username;
       <div key={`msg_${index}`} style={{ width: "100%" }}>
         <div className="message-block">
-          {isMyMessage ? <MyMessage /> : <TheirMessage />}
+          {isMyMessage ? (
+            <MyMessage message={message} />
+          ) : (
+            <TheirMessage
+              message={message}
+              lastMessage={messages[lastMessageKey]}
+            />
+          )}
         </div>
         <div
           className="read-receipts"
@@ -26,13 +33,17 @@ const ChatFeed = (props) => {
       </div>;
     });
   };
-  renderMessages();
+
   if (!chat) return "...Loading";
   return (
     <div className="chat-feed">
       <div className="chat-title-container">
         <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => `${person.person.username}`)}
+        </div>
       </div>
+      {renderMessages()}
     </div>
   );
 };
